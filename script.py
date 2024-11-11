a4b45e04256e1048928ac832732f800bfc6e8fad
alexpeain <tonghsengsom@gmail.com>
2024-11-11 22:52:11 +0630

    add handle errors for file

--- script.py
+++ script.py
@@ -5,10 +5,22 @@ import os
 task_file = "tasks.json"
 def load_tasks():
     if os.path.exists(task_file):
-        with open(task_file,'r')as file:
-            return json.load(file)
-    return []
-
+        try:
+            with open(task_file, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("No existing task file found. A new one will be created.")
+            return []
+        except json.JSONDecodeError:
+            print("Error decoding JSON from the task file. The file may be corrupted.")
+            return []
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return []
+    else:
+        print("No existing task file found. A new one will be created.")
+        return []
+    
 def save_tasks(tasks):
     with open(task_file, 'w')as file:
         json.dump(tasks,file)
@@ -19,6 +31,7 @@ parser.add_argument("--tasklist", help="Custom tasks", default="Task-cli")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("--add",help ="the task to add")
 group.add_argument("--list",help ="List all tasks")
+parser.add_argument('update', nargs='?', help='Edit a task by ID')
 parser.add_argument("--format", choices = ["json"], help = "Output format")
 args = parser.parse_args()
 
@@ -37,3 +50,5 @@ if args.list:
         
 if args.format == "json" and args.list:
     print(json.dumps(tasks, indent=2))
+
+
