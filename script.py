9a516da41c564ca8a996d728143f202756ab0484
alexpeain <tonghsengsom@gmail.com>
2024-11-17 21:31:20 +0630

    add delete

--- script.py
+++ script.py
@@ -28,6 +28,7 @@ group = parser.add_mutually_exclusive_group(required=False)
 group.add_argument("--add", help="The task to add")
 group.add_argument("--list", action='store_true', help="List all tasks")
 parser.add_argument("--update", help="Edit a task by providing new task description")
+parser.add_argument("--delete",action='store_true',help ="Delete Tasks")
 parser.add_argument("-i", "--index", type=int, help="The index of the task to update")
 parser.add_argument("--format", choices=["json"], help="Output format")
 args = parser.parse_args()
@@ -55,6 +56,16 @@ if args.update is not None and args.index is not None:
         print(f"Updated task {old_task} to {args.update}.")
     else:
         print("Invalid index. Please provide a valid task index.")
+
+#Delete a task
+if args.delete and args.index is not None:
+    if 0 < args.index < len(tasks):
+        removed_task = tasks.pop(args.index)
+        save_tasks(tasks)
+        print(f"Deleted task: {removed_task}.")
+    else:
+        print("Invalid index. Please provide a valid task index.")
+        
 
 # Output in JSON format if specified
 if args.format == "json" and args.list:
