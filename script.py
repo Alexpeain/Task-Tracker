b17441c581980ad055be7f2683fb24cd125d66d8
alexpeain <tonghsengsom@gmail.com>
2024-11-19 22:57:18 +0630

    Add Mark and createdat & updatedat

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+from datetime import datetime
 
 task_file = "tasks.json"
 
@@ -29,6 +30,10 @@ group.add_argument("--add", help="The task to add")
 group.add_argument("--list", action='store_true', help="List all tasks")
 parser.add_argument("--update", help="Edit a task by providing new task description")
 parser.add_argument("--delete",action='store_true',help ="Delete Tasks")
+parser.add_argument("--delete-all", action='store_true', help="Delete all tasks")
+parser.add_argument("--mark-done", action='store_true',help="Mark as Done")
+parser.add_argument("--mark-todo",action='store_true',help ="Mark Todo")
+parser.add_argument("--mark-in-progress",action='store_true',help="Mark In Progress")
 parser.add_argument("-i", "--index", type=int, help="The index of the task to update")
 parser.add_argument("--format", choices=["json"], help="Output format")
 args = parser.parse_args()
@@ -37,7 +42,11 @@ tasks = load_tasks()
 
 # Add a new task
 if args.add:
-    tasks.append(args.add)
+    now = datetime.now().isoformat()
+    tasks.append({"description": args.add,
+                  "status": "TODO",
+                  "createdAt": now,
+                  "updatedAt": now })
     save_tasks(tasks)
     print(f"{args.tasklist} added: {args.add}")
 
@@ -45,13 +54,15 @@ if args.add:
 if args.list:
     print("Current tasks:")
     for idx, task in enumerate(tasks, start=1):
-        print(f"{args.tasklist} {idx}. {task}")
+        print(f"{args.tasklist} {idx}. {task['description']} [{task['status']}] "
+              f"(Created at: {task['createdAt']}, Updated at: {task['updatedAt']})")
 
 # Update a task
 if args.update is not None and args.index is not None:
     if 0 <= args.index < len(tasks):
         old_task = tasks[args.index]
-        tasks[args.index] = args.update
+        tasks[args.index]['description'] = args.update
+        tasks[args.index]['updatedAt'] =datetime.now().isoformat()
         save_tasks(tasks)
         print(f"Updated task {old_task} to {args.update}.")
     else:
@@ -62,11 +73,33 @@ if args.delete and args.index is not None:
     if 0 < args.index < len(tasks):
         removed_task = tasks.pop(args.index)
         save_tasks(tasks)
-        print(f"Deleted task: {removed_task}.")
+        print(f"Deleted task: {removed_task['description']}.")
     else:
         print("Invalid index. Please provide a valid task index.")
-        
+#Delete all tasks
+if args.delete_all:
+    tasks.clear()
+    save_tasks(tasks)
+    print("All tasks have been deleted.")
 
+#Mark Done,todo, in-progress
+if args.mark_done and  args.index is not None:
+    if 0 < args.index < len(tasks):
+        tasks[args.index]['status'] = 'done'
+        save_tasks(tasks)
+        print(f"Task marked as done: {tasks[args.index]['description']}.")
+    else:
+        print("Invalid index.")
+
+if args.mark_in_progress and args.index is not None:
+    if 0 <= args.index < len(tasks):
+        tasks[args.index]['status'] = 'IN_PROGRESS'
+        save_tasks(tasks)
+        print(f"Task marked as in progress: {tasks[args.index]['description']}.")
+    else:
+        print("Invalid index.")
+
+        
 # Output in JSON format if specified
 if args.format == "json" and args.list:
     print(json.dumps(tasks, indent=2))
