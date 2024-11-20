c1412eae1aa453eb5342635c9c8992eb48351da4
alexpeain <tonghsengsom@gmail.com>
2024-11-20 21:46:38 +0630

    list done in_progress todo

--- script.py
+++ script.py
@@ -28,6 +28,9 @@ parser.add_argument("--tasklist", help="Custom tasks", default="Task-cli")
 group = parser.add_mutually_exclusive_group(required=False)
 group.add_argument("--add", help="The task to add")
 group.add_argument("--list", action='store_true', help="List all tasks")
+parser.add_argument("--list-done",action="store_true",help="List done task")
+parser.add_argument("--list-in-progress",action="store_true",help="List in progres task")
+parser.add_argument("--list-todo",action="store_true",help="List todo tasks")
 parser.add_argument("--update", help="Edit a task by providing new task description")
 parser.add_argument("--delete",action='store_true',help ="Delete Tasks")
 parser.add_argument("--delete-all", action='store_true', help="Delete all tasks")
@@ -57,6 +60,36 @@ if args.list:
         print(f"{args.tasklist} {idx}. {task['description']} [{task['status']}] "
               f"(Created at: {task['createdAt']}, Updated at: {task['updatedAt']})")
 
+#ListDone
+if args.list_done:
+    print("Complete tasks:")
+    completed_tasks = [task for task in tasks if task['status'] == 'Done']
+    if completed_tasks:
+        for idx,task in enumerate(completed_tasks, start=1):
+             print(f"{args.tasklist} {idx}. {task['description']} - Status: {task['status']}")
+    else:
+        print("No completed tasks found.")
+        
+#list-in-Progress 
+if args.list_in_progress:
+    print("List in Progress: ")
+    in_progress_tasks = [task for task in tasks if task['status'] == 'IN_PROGRESS']
+    if in_progress_tasks:
+        for idx,task in enumerate(in_progress_tasks,start=1):
+            print(f"{args.tasklist} {idx}. {task['description']} - Status: {task['status']}")
+    else:
+        print("No in progress tasks found.")
+
+#list-todo
+if args.list_todo:
+    print("List Todo")
+    todo_tasks = [task for task in tasks if task['status'] == 'TODO']
+    if todo_tasks:
+        for idx,task in enumerate(todo_tasks,start=1):
+            print(f"{args.tasklist} {idx}. {task['description']} - Status: {task['status']}")
+    else:
+        print("No To Do tasks found.")
+            
 # Update a task
 if args.update is not None and args.index is not None:
     if 0 <= args.index < len(tasks):
@@ -85,7 +118,7 @@ if args.delete_all:
 #Mark Done,todo, in-progress
 if args.mark_done and  args.index is not None:
     if 0 < args.index < len(tasks):
-        tasks[args.index]['status'] = 'done'
+        tasks[args.index]['status'] = 'Done'
         save_tasks(tasks)
         print(f"Task marked as done: {tasks[args.index]['description']}.")
     else:
@@ -99,7 +132,14 @@ if args.mark_in_progress and args.index is not None:
     else:
         print("Invalid index.")
 
-        
+if args.mark_todo and args.index is not None:
+    if 0 <= args.index < len(tasks):
+        tasks[args.index]['status'] == 'TODO'
+        save_tasks(tasks)
+        print(f"Task marked as To Do: {tasks[args.index]['description']}.")
+    else:
+        print("Invalid index.")
+            
 # Output in JSON format if specified
 if args.format == "json" and args.list:
     print(json.dumps(tasks, indent=2))
